89e8740c043e44ae5f44e24db3a98b25b54811d6
Collin Palm <collinpalm@gmail.com>
2021-10-04 17:34:55 -0400

    routing works

--- resumeSite/src/app/app-routing.module.ts
+++ resumeSite/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-
+import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent}
+  {path: 'home', component: HomePageComponent},
+  {path: 'about-me', component: AboutMeComponent}
 ];
 
 @NgModule({
