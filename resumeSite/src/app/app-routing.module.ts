9b4ba699929a8b31913cbbcc54db9a705bba2574
Collin Palm <collinpalm@gmail.com>
2021-10-04 15:48:55 -0400

    added aboutme page

--- resumeSite/src/app/app-routing.module.ts
+++ resumeSite/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
