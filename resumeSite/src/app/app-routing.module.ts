ee6d3fa6e8ef576d6349e16da95f2ecb4b5a89a7
Collin Palm <collinpalm@gmail.com>
2021-10-04 20:44:53 -0400

    editing css

--- resumeSite/src/app/app-routing.module.ts
+++ resumeSite/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { AboutMeComponent } from './about-me/about-me.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomePageComponent},
-  {path: 'about-me', component: AboutMeComponent}
+  {path: 'home', component: HomePageComponent}
 ];
 
 @NgModule({
--- resumeSite/src/app/app.module.ts
+++ resumeSite/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
-import { AboutMeComponent } from './about-me/about-me.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    AboutMeComponent
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
