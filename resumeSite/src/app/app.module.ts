291801b56282f68249e00d39be878294b1dbf2a7
Collin Palm <collinpalm@gmail.com>
2021-10-06 16:49:13 -0400

    ugh

--- resumeSite/src/app/app.module.ts
+++ resumeSite/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
-import { ThemeSwitchComponent } from './theme-switch/theme-switch.component';
+import { ThemeSwitchComponent } from './shared/theme-switch/theme-switch.component';
 
 
 @NgModule({
