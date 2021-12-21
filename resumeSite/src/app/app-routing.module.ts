0a3fe5ba51f9dfd925748ea9ccc3ed03c6fc1a6a
Collinpalm <collinpalm@gmail.com>
2021-12-21 13:00:10 -0500

    added contact me with routing

--- resumeSite/src/app/app-routing.module.ts
+++ resumeSite/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'projects', component: ProjectsComponent}
+  { path: 'projects', component: ProjectsComponent},
+  { path: 'contact-me', component: ContactMeComponent}
 ];
 
 @NgModule({
