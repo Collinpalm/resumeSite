c470f6c27d855b3eb59e9067aa6047ff09c523ef
Collinpalm <collinpalm@gmail.com>
2021-12-31 16:32:44 -0500

    changes...changes

--- resumeSite/src/app/contact-me/contact-me.component.ts
+++ resumeSite/src/app/contact-me/contact-me.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
@@ -12,5 +18,8 @@ export class ContactMeComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  tiles: Tile[] = [
+    {text: 'new personal email', cols: 1, rows: 1},
+    {text: 'collinpalm@knights.ucf.edu', cols: 1, rows: 1},
+  ];
 }
